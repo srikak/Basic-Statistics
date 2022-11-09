29dfd3a1df9d4441b43b71defdd3cb975c65710a
srikak <95039406+srikak@users.noreply.github.com>
2022-11-09 12:08:06 +0530

    Create demo_centraltendencies.py

---
+++ demo_centraltendencies.py
@@ -0,0 +1,9 @@
+# # Input from STDIN
+# array = list(map(int, input().split()))
+
+# Or...
+array = [1,2,3,4,1,5,0,5,4,6,7] 
+
+print("{:.1f}".format(mean(array)))
+print("{:.1f}".format(median(array)))
+print(mode(array))
